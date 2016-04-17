d06b96b67d05f39f967cc0f9c7d8cf427c59aa0c
Neville Camilleri <neville.camilleri@tipico.com>
2016-04-17 20:57:48 +0200

    backoffice-api: [Added CRUD in controller with Jackson JSON mapper for campaigns]

--- backoffice-api/src/main/java/com/tipico/social/marketing/controller/CampaignsController.java
+++ backoffice-api/src/main/java/com/tipico/social/marketing/controller/CampaignsController.java
@@ -2,14 +2,16 @@ package com.tipico.social.marketing.controller;
 
 import com.tipico.social.marketing.contract.Campaign;
 import com.tipico.social.marketing.repository.CampaignsProvider;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 /**
  * Created by ncamilleri on 16/04/16.
@@ -28,10 +30,53 @@ public class CampaignsController {
         return "Hello, Campaigns REST API is working!";
     }
 
-    @RequestMapping("/getCampaignDetails")
+    @RequestMapping("/getCampaign")
     @ResponseBody
-    public Campaign getCampaignDetails(@RequestParam(value="id", required=true) String id) {
+    public Campaign getCampaign(@RequestParam(value="id", required=true) String id) {
         return campaignsProvider.findOne(id);
+    }
+
+    @RequestMapping("/saveCampaign")
+    @ResponseBody
+    public Campaign saveCampaign(@RequestParam(value="campaign", required=true) String campaignJson) {
+        try {
+            Campaign campaign = new ObjectMapper().readValue(campaignJson, Campaign.class);
+            return campaignsProvider.insert(campaign);
+        } catch (JsonParseException ioe) {
+            ioe.printStackTrace();
+        } catch (JsonMappingException ioe) {
+            ioe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping("/deleteCampaign")
+    @ResponseBody
+    public void deleteCampaign(@RequestParam(value="id", required=true) String id) {
+        try {
+            Campaign campaign = campaignsProvider.findOne(id);
+            campaignsProvider.delete(campaign);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping("/updateCampaign")
+    @ResponseBody
+    public Campaign updateCampaign(@RequestParam(value="campaign", required=true) String campaignJson) {
+        try {
+            Campaign campaign = new ObjectMapper().readValue(campaignJson, Campaign.class);
+            return campaignsProvider.save(campaign);
+        } catch (JsonParseException ioe) {
+            ioe.printStackTrace();
+        } catch (JsonMappingException ioe) {
+            ioe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return null;
     }
 
     public static void main(String[] args) throws Exception {
