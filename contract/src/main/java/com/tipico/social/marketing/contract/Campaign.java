2ef08a08408e7ed8933ebddb83f02bd3507faa95
Neville Camilleri <neville.camilleri@gmail.com>
2016-04-23 14:21:41 +0200

    master: Changed contract Id to String type from Integer

--- contract/src/main/java/com/tipico/social/marketing/contract/Campaign.java
+++ contract/src/main/java/com/tipico/social/marketing/contract/Campaign.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Campaign {
 
-    private Integer id;
+    private String id;
     private String eventName;
     private Date created;
     private Date lastUpdated;
@@ -13,11 +13,11 @@ public class Campaign {
     private int delayBetweenPosts;
     private String message;
 
-    public Integer getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(String id) {
         this.id = id;
     }
 
